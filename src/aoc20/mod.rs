9f68a8aa26e6225153c97b890c930b3b3a4158d8
KaeL <uberkael@gmail.com>
2024-10-11 16:19:31 +0100

    20 Part 1 - Firewall Rules

--- src/aoc20/mod.rs
+++ src/aoc20/mod.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use rayon::prelude::*;
+
 #[cfg(test)]
 mod tests;
 
@@ -10,5 +12,32 @@ pub fn aoc20() {
 	let input = std::fs::read_to_string("input/20/input.txt").unwrap();
 
 	println!("Part 1:\n{}", part1(&input));
-	println!("Part 2:\n{}", part2(&input));
+	// println!("Part 2:\n{}", part2(&input));
+}
+
+fn ranges(input: String) -> Vec<bool> {
+	let mut ips = vec![true; 4294967296];
+	for line in input.lines() {
+		let parts: Vec<&str> = line.split('-').collect();
+		let start = parts[0].parse::<u32>().unwrap();
+		let end = parts[1].parse::<u32>().unwrap();
+		// Divide el rango en chunks y procesa en paralelo
+		ips[start as usize..=end as usize]
+			.par_chunks_mut(1024) // Divide en chunks
+			.for_each(|chunk| {
+				for i in chunk.iter_mut() {
+					*i = false;
+				}
+			});
+	}
+	ips
+}
+
+fn ip_min(ips: Vec<bool>) -> u32 {
+	ips.iter().position(|&x| x).unwrap() as u32
+}
+
+fn part1(input: &String) -> u32 {
+	let ips = ranges(input.to_string());
+	ip_min(ips)
 }
--- src/aoc20/tests.rs
+++ src/aoc20/tests.rs
@@ -1,3 +1,18 @@
 use super::*;
 
-// #[test]
+#[test]
+fn test_ranges() {
+	assert_eq!(ranges("5-8\n0-2\n4-7".to_string()).len(), 4294967295);
+}
+
+#[test]
+fn test_min() {
+	let ips = ranges("5-8\n0-2\n4-7".to_string());
+	assert_eq!(ip_min(ips), 3);
+}
+
+#[test]
+fn test_min2() {
+	let ips = ranges("0-10000".to_string());
+	assert_eq!(ip_min(ips), 10001);
+}
