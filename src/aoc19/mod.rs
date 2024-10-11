19e1714ec45b651dcf065ea27c92f2db55a91128
KaeL <uberkael@gmail.com>
2024-10-11 14:51:33 +0100

    19 Part 2 - An Elephant Named Joseph

--- src/aoc19/mod.rs
+++ src/aoc19/mod.rs
@@ -10,25 +10,35 @@ pub fn aoc19() {
 	let input = std::fs::read_to_string("input/19/input.txt").unwrap();
 
 	println!("Part 1:\n{}", part1(&input));
+	println!("Part 2:\n{}", part2(&input));
 }
 
 /* 2(n−2^(log_2(n)⌋)+1) */
-fn josephus(input: usize) -> usize {
-	// Encontrar la mayor potencia de 2 menor o igual a input
-	let mut largest_power = 1;
-	while largest_power * 2 <= input {
-		largest_power *= 2;
-	}
-	// Aplicar la fórmula del problema de Josephus
-	let l = input - largest_power;
-	if l == 0 {
-		largest_power
-	} else {
-		2 * l + 1
-	}
+fn josephus(n: usize) -> usize {
+	let z = (n as f64).log2().floor();
+	2 * (n - 2usize.pow(z as u32)) + 1
 }
 
 fn part1(input: &str) -> usize {
 	let input = input.trim().parse().unwrap();
 	josephus(input)
+}
+
+/* 3^⌊log_3(n)⌋ si n es potencia de 3
+ * n−3^⌊log_3(n)⌋ si n está entre 3^⌊log_3(n)⌋ y 2*3^⌊log_3(n)⌋
+ * 2n−3^⌊log_3(n)⌋ si n está entre 2*3^⌊log_3(n)⌋ y 3*3^⌊log_3(n)⌋ */
+fn josephus_opposite(n: usize) -> usize {
+	let z = (n as f64).log(3.0).floor() as u32;
+	if n == 3usize.pow(z) {
+		3usize.pow(z)
+	} else if n <= 2 * 3usize.pow(z) {
+		n - 3usize.pow(z)
+	} else {
+		(2 * n) - 3usize.pow(z)
+	}
+}
+
+fn part2(input: &str) -> usize {
+	let n: usize = input.trim().parse().unwrap();
+	josephus_opposite(n)
 }
--- src/aoc19/tests.rs
+++ src/aoc19/tests.rs
@@ -5,3 +5,9 @@ fn test_josephus() {
 	assert_eq!(josephus(5), 3);
 	assert_eq!(josephus(10), 5);
 }
+
+#[test]
+fn test_josephus_opposite() {
+	assert_eq!(josephus_opposite(5), 2);
+	assert_eq!(josephus_opposite(10), 1);
+}
