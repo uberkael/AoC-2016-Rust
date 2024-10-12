03f9dc0484501bfe16d0d3547ee7806a0cfc219e
KaeL <uberkael@gmail.com>
2024-10-12 12:56:24 +0100

    21 Part 1 - Scrambled Letters and Hash
    - Reorganize

--- src/aoc21/mod.rs
+++ src/aoc21/mod.rs
@@ -23,96 +23,111 @@ enum Operation {
 	Move(usize, usize),
 }
 
-fn parse_operation(input: &str) -> Option<Operation> {
-	let parts: Vec<&str> = input.split_whitespace().collect();
-	match (parts[0], parts[1]) {
-		("swap", "position") => {
-			let x = parts[2].parse::<usize>().ok()?;
-			let y = parts[5].parse::<usize>().ok()?;
-			Some(Operation::SwapPosition(x, y))
+impl Operation {
+	fn parse(input: &str) -> Option<Self> {
+		let parts: Vec<&str> = input.split_whitespace().collect();
+		match (parts[0], parts[1]) {
+			("swap", "position") => {
+				let x = parts[2].parse::<usize>().ok()?;
+				let y = parts[5].parse::<usize>().ok()?;
+				Some(Operation::SwapPosition(x, y))
+			}
+			("swap", "letter") => {
+				let x = parts[2].chars().next()?;
+				let y = parts[5].chars().next()?;
+				Some(Operation::SwapLetter(x, y))
+			}
+			("rotate", "left") => {
+				let x = parts[2].parse::<usize>().ok()?;
+				Some(Operation::RotateLeft(x))
+			}
+			("rotate", "right") => {
+				let x = parts[2].parse::<usize>().ok()?;
+				Some(Operation::RotateRight(x))
+			}
+			("rotate", "based") => {
+				let x = parts[6].chars().next()?;
+				Some(Operation::RotateOnLetter(x))
+			}
+			("reverse", _) => {
+				let x = parts[2].parse::<usize>().ok()?;
+				let y = parts[4].parse::<usize>().ok()?;
+				Some(Operation::Reverse(x, y))
+			}
+			("move", _) => {
+				let x = parts[2].parse::<usize>().ok()?;
+				let y = parts[5].parse::<usize>().ok()?;
+				Some(Operation::Move(x, y))
+			}
+			_ => None,
 		}
-		("swap", "letter") => {
-			let x = parts[2].chars().next()?;
-			let y = parts[5].chars().next()?;
-			Some(Operation::SwapLetter(x, y))
+	}
+	fn run(self, password: String) -> String {
+		match self {
+			Operation::SwapPosition(x, y) => swap_position(password, x, y),
+			Operation::SwapLetter(x, y) => swap_letter(password, x, y),
+			Operation::RotateLeft(x) => rotate_left(password, x),
+			Operation::RotateRight(x) => rotate_right(password, x),
+			Operation::RotateOnLetter(x) => rotate_on_letter(password, x),
+			Operation::Reverse(x, y) => reverse(password, x, y),
+			Operation::Move(x, y) => p_move(password, x, y),
 		}
-		("rotate", "left") => {
-			let x = parts[2].parse::<usize>().ok()?;
-			Some(Operation::RotateLeft(x))
-		}
-		("rotate", "right") => {
-			let x = parts[2].parse::<usize>().ok()?;
-			Some(Operation::RotateRight(x))
-		}
-		("rotate", "based") => {
-			let x = parts[6].chars().next()?;
-			Some(Operation::RotateOnLetter(x))
-		}
-		("reverse", _) => {
-			let x = parts[2].parse::<usize>().ok()?;
-			let y = parts[4].parse::<usize>().ok()?;
-			Some(Operation::Reverse(x, y))
-		}
-		("move", _) => {
-			let x = parts[2].parse::<usize>().ok()?;
-			let y = parts[5].parse::<usize>().ok()?;
-			Some(Operation::Move(x, y))
-		}
-		_ => None,
 	}
 }
 
-fn run_operation(password: String, op: Operation) -> String {
-	match op {
-		Operation::SwapPosition(x, y) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			chars.swap(x, y);
-			chars.iter().collect::<String>()
-		}
-		Operation::SwapLetter(x, y) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			let x_pos = chars.iter().position(|&c| c == x).unwrap();
-			let y_pos = chars.iter().position(|&c| c == y).unwrap();
-			chars.swap(x_pos, y_pos);
-			chars.iter().collect::<String>()
-		}
-		Operation::RotateLeft(x) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			chars.rotate_left(x);
-			chars.iter().collect::<String>()
-		}
-		Operation::RotateRight(x) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			chars.rotate_right(x);
-			chars.iter().collect::<String>()
-		}
-		Operation::RotateOnLetter(x) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			let pos = chars.iter().position(|&c| c == x).unwrap();
-			let n = 1 + pos + if pos >= 4 { 1 } else { 0 };
-			let shift = n % chars.len();
-			chars.rotate_right(shift);
-			chars.iter().collect::<String>()
-		}
-		Operation::Reverse(x, y) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			chars[x..=y].reverse();
-			chars.iter().collect::<String>()
-		}
-		Operation::Move(x, y) => {
-			let mut chars: Vec<char> = password.chars().collect();
-			let c = chars.remove(x);
-			chars.insert(y, c);
-			chars.iter().collect::<String>()
-		}
-	}
+fn swap_position(password: String, x: usize, y: usize) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	chars.swap(x, y);
+	chars.iter().collect::<String>()
+}
+
+fn swap_letter(password: String, x: char, y: char) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	let x_pos = chars.iter().position(|&c| c == x).unwrap_or_default();
+	let y_pos = chars.iter().position(|&c| c == y).unwrap_or_default();
+	chars.swap(x_pos, y_pos);
+	chars.iter().collect::<String>()
+}
+
+fn rotate_left(password: String, x: usize) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	chars.rotate_left(x);
+	chars.iter().collect::<String>()
+}
+
+fn rotate_right(password: String, x: usize) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	chars.rotate_right(x);
+	chars.iter().collect::<String>()
+}
+
+fn rotate_on_letter(password: String, x: char) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	let pos = chars.iter().position(|&c| c == x).unwrap_or_default();
+	let n = 1 + pos + if pos >= 4 { 1 } else { 0 };
+	let shift = n % chars.len();
+	chars.rotate_right(shift);
+	chars.iter().collect::<String>()
+}
+
+fn reverse(password: String, x: usize, y: usize) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	chars[x..=y].reverse();
+	chars.iter().collect::<String>()
+}
+
+fn p_move(password: String, x: usize, y: usize) -> String {
+	let mut chars: Vec<char> = password.chars().collect();
+	let c = chars.remove(x);
+	chars.insert(y, c);
+	chars.iter().collect::<String>()
 }
 
 fn part1(input: &str) -> String {
 	let mut password = "abcdefgh".to_string();
 	for line in input.lines() {
-		if let Some(op) = parse_operation(line) {
-			password = run_operation(password, op);
+		if let Some(op) = Operation::parse(line) {
+			password = op.run(password);
 		}
 	}
 	password
