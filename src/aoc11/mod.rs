996dc3ea7e6d237bb00a5d7a4848815100b822ca
KaeL <uberkael@gmail.com>
2024-10-21 13:26:41 +0100

    Refactor Day 11
    - Better BFS
    - State fixed array
    - Validation mod
    - Finalize

--- src/aoc11/mod.rs
+++ src/aoc11/mod.rs
@@ -7,114 +7,145 @@ pub fn aoc11() {
 	println!("Part 2:\n{}", part2());
 }
 
-/*  */
-#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+struct Floor {
+	gens: u8,
+	chips: u8,
+}
+
+impl Floor {
+	#[inline]
+	fn new(gens: u8, chips: u8) -> Self {
+		Floor { gens, chips }
+	}
+	#[inline]
+	fn valid(&self) -> bool {
+		self.gens == 0 || self.gens >= self.chips
+	}
+	#[inline]
+	fn total(&self) -> u8 {
+		self.gens + self.chips
+	}
+	#[inline]
+	fn add(&self, other: &Floor) -> Floor {
+		Floor::new(self.gens + other.gens, self.chips + other.chips)
+	}
+	#[inline]
+	fn sub(&self, other: &Floor) -> Floor {
+		Floor::new(self.gens - other.gens, self.chips - other.chips)
+	}
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 struct State {
 	elevator: u8,
-	pairs: Vec<(u8, u8)>,
+	floors: [Floor; 4],
 }
 
 impl State {
-	fn sort(&mut self) {
-		self.pairs.sort();
+	fn new(pairs: Vec<(u8, u8)>) -> Self {
+		let mut floors = [Floor::new(0, 0); 4];
+		for (chip, gen) in pairs {
+			// Restamos 1 para usar índices 0-based.
+			floors[(chip - 1) as usize].chips += 1;
+			floors[(gen - 1) as usize].gens += 1;
+		}
+		State {
+			elevator: 1,
+			floors,
+		}
 	}
 	fn is_valid(&self) -> bool {
-		self.pairs.iter().all(|&(chip, gen)| {
-			chip == gen || !self.pairs.iter().any(|&(_, other_gen)| other_gen == chip)
-		})
-	}
-	fn is_finished(&self, top: u8) -> bool {
-		self.pairs.iter().all(|&(c, g)| c == top && g == top)
-	}
-	fn items_on_elevator(&self) -> Vec<(usize, bool)> {
-		let mut items = Vec::new();
-		for (i, &(chip, gen)) in self.pairs.iter().enumerate() {
-			if chip == self.elevator {
-				items.push((i, true));
-			}
-			if gen == self.elevator {
-				items.push((i, false));
-			}
-		}
-		items
-	}
-	fn new(pairs: Vec<(u8, u8)>) -> Self {
-		let mut init = State {
-			elevator: 1,
-			pairs: pairs,
-		};
-		init.sort();
-		init
-	}
-	fn gen(&self, combo: &Vec<(usize, bool)>, dest: u8) -> State {
-		let mut new_state = self.clone();
-		new_state.elevator = dest;
-		combo.iter().for_each(|&(idx, is_chip)| {
-			if is_chip {
-				new_state.pairs[idx].0 = dest;
-			} else {
-				new_state.pairs[idx].1 = dest;
-			}
-		});
-		new_state.sort();
-		new_state
+		self.floors.iter().all(|f| f.valid())
 	}
 }
 
-fn bfs(init: State, top: u8) -> usize {
+const MOVES: [Floor; 5] = [ // Posibles movimientos
+	Floor { gens: 2, chips: 0 },
+	Floor { gens: 1, chips: 1 },
+	Floor { gens: 0, chips: 2 },
+	Floor { gens: 1, chips: 0 },
+	Floor { gens: 0, chips: 1 },
+];
+
+fn bfs(init: State) -> usize {
+	let complete: u8 = init.floors.iter().map(|f| f.total()).sum();
 	let mut visited = HashSet::new();
 	let mut queue = VecDeque::new();
-	queue.push_back((init.clone(), 0));
+	queue.push_back((init, 0));
 	visited.insert(init);
 	while let Some((state, steps)) = queue.pop_front() {
-		if state.is_finished(top) {
+		if state.floors[3].total() == complete {
 			return steps;
 		}
-		// Posibles direcciones de movimiento.
-		let mut directions = Vec::new();
-		if state.elevator < top {
-			directions.push(state.elevator + 1);
-		}
-		if state.elevator > 1 {
-			directions.push(state.elevator - 1);
-		}
-		// Obtiene los ítems que se pueden mover (microchips o generadores en el piso actual).
-		let items = state.items_on_elevator();
-		// Genera todas las combinaciones posibles de 1 o 2 ítems.
-		let combos = combinations(items);
-		// Para cada dirección y cada combinación, genera el nuevo estado.
-		for dest in directions {
-			combos.iter().for_each(|combo| {
-				let new_state = state.gen(combo, dest);
-				if new_state.is_valid() && !visited.contains(&new_state) {
-					visited.insert(new_state.clone());
-					queue.push_back((new_state, steps + 1));
+		let cur = (state.elevator - 1) as usize;
+		if state.elevator > 1 { // DOWN
+			let can_move_down = (0..cur).any(|i| state.floors[i].total() > 0);
+			if can_move_down {
+				let dest = state.elevator - 1;
+				let dest_idx = (dest - 1) as usize;
+				let mut min_moved = 3; // Máximo 2
+				for m in MOVES.iter().rev() {
+					if m.total() > min_moved
+					{ break; }
+					if cant_move(cur, m, state)
+					{ continue; } // Not enough items
+					let mut next = state.clone();
+					next.elevator = dest;
+					next.floors[cur] = next.floors[cur].sub(m);
+					next.floors[dest_idx] = next.floors[dest_idx].add(m);
+					if not_valid(cur, dest_idx, next)
+					{ continue; }
+					if visited.insert(next.clone()) {
+						min_moved = m.total();
+						queue.push_back((next, steps + 1));
+					}
 				}
-			});
+			}
+		}
+		if state.elevator < 4 { // UP
+			let dest = state.elevator + 1;
+			let dest_idx = (dest - 1) as usize;
+			let mut max_moved = 0;
+			for m in MOVES.iter() {
+				if m.total() < max_moved
+				{ break; }
+				if cant_move(cur, m, state)
+				{ continue; } // Not enough items
+				let mut next = state.clone();
+				next.elevator = dest;
+				next.floors[cur] = next.floors[cur].sub(m);
+				next.floors[dest_idx] = next.floors[dest_idx].add(m);
+				if not_valid(cur, dest_idx, next)
+				{	continue; } // Estado inválido, saltar
+				if visited.insert(next.clone()) {
+					max_moved = m.total();
+					queue.push_back((next, steps + 1));
+				}
+			}
 		}
 	}
-	unreachable!();
+	panic!("No solution found");
 }
 
-fn combinations(items: Vec<(usize, bool)>) -> Vec<Vec<(usize, bool)>> {
-	let mut combos = Vec::new();
-	for i in 0..items.len() {
-		combos.push(vec![items[i]]);
-	}
-	for i in 0..items.len() {
-		for j in (i + 1)..items.len() {
-			combos.push(vec![items[i], items[j]]);
-		}
-	}
-	combos
+#[inline]
+fn cant_move(cur: usize, m: &Floor, state: State) -> bool {
+		state.floors[cur].gens < m.gens || state.floors[cur].chips < m.chips
+}
+
+#[inline]
+fn not_valid(cur: usize, dest_idx: usize, next: State) -> bool {
+		!next.floors[cur].valid() || !next.floors[dest_idx].valid() || !next.is_valid()
 }
 
 pub fn part1() -> usize {
 	let state = State::new(vec![(2, 1), (1, 1), (2, 1), (1, 1), (1, 1)]);
-	bfs(state, 4)
+	bfs(state)
 }
 
 pub fn part2() -> usize {
-	let state = State::new(vec![(2, 1), (1, 1), (2, 1), (1, 1), (1, 1), (1, 1), (1, 1)]);
-	bfs(state, 4)
+	let mut state = State::new(vec![(2, 1), (1, 1), (2, 1), (1, 1), (1, 1)]);
+	state.floors[0].chips += 2;
+	state.floors[0].gens += 2;
+	bfs(state)
 }
