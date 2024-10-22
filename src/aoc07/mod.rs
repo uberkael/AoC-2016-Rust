0cdea662710fa4a08dfa576f3587cb6d660ef9f6
KaeL <uberkael@gmail.com>
2024-10-22 16:23:02 +0100

    Day 07 Performance
    - Regex -> Chars
    - Parallelize Rayon

--- src/aoc07/mod.rs
+++ src/aoc07/mod.rs
@@ -1,40 +1,44 @@
+use rayon::prelude::*;
+
 #[cfg(test)]
 mod tests;
-
-use regex::Regex;
 
 pub fn aoc07() {
 	println!("\nDay 7: Internet Protocol Version 7");
 	println!("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
 
-	let input = std::fs::read_to_string("input/07/input.txt")
-		.expect("Error reading file");
+	let input = std::fs::read_to_string("input/07/input.txt").expect("Error reading file");
 
-	/* Parte 1 */
-	println!("Part 1:\n{}", part1(&input, false));
-
-	/* Parte 2 */
-	println!("Part 2:\n{}", part2(&input, false));
-
+	println!("Part 1:\n{}", part1(&input));
+	println!("Part 2:\n{}", part2(&input));
 }
 
-fn part1(input: &str, calc: bool) -> usize {
-	if !calc { return 118; }
-	input.lines().filter(|l| check_tls(l)).count()
+fn part1(input: &str) -> usize {
+	input.par_lines().filter(|l| check_tls(l)).count()
 }
 
 fn check_palindrome(s: &str) -> bool {
-	if s.len() != 4 { return false; }
-	s[0..1] == s[3..4] && s[1..2] == s[2..3] && s[0..1] != s[1..2]
+	s.len() == 4
+		&& s.as_bytes()[0] == s.as_bytes()[3]
+		&& s.as_bytes()[1] == s.as_bytes()[2]
+		&& s.as_bytes()[0] != s.as_bytes()[1]
 }
 
 fn check_hypernet(s: &str) -> bool {
-	let re_extract = Regex::new(r"\[(.*?)\]").expect("Invalid regex");
-	for m in re_extract.find_iter(s) {
-		let ss = m.as_str();
-		let ss = &ss[1..ss.len()-1];
-		for i in 0..ss.len()-3 {
-			if check_palindrome(&ss[i..i+4]) {
+	let mut inside_brackets = false;
+	let mut start_index = 0;
+
+	for (i, c) in s.chars().enumerate() {
+		if c == '[' {
+			inside_brackets = true;
+			start_index = i;
+		} else if c == ']' {
+			inside_brackets = false;
+			if check_palindrome(&s[start_index + 1..i]) {
+				return true;
+			}
+		} else if inside_brackets && i >= start_index + 3 {
+			if check_palindrome(&s[i - 3..i + 1]) {
 				return true;
 			}
 		}
@@ -43,11 +47,17 @@ fn check_hypernet(s: &str) -> bool {
 }
 
 fn check_abba(s: &str) -> bool {
-	let re_cut = Regex::new(r"\[.*?\]").expect("Invalid regex");
-	let parts: Vec<&str> = re_cut.split(s).collect();
-	for part in parts {
-		for i in 0..part.len()-3 {
-			if check_palindrome(&part[i..i+4]) {
+	let mut inside_brackets = false;
+	let mut start_index = 0;
+
+	for (i, c) in s.chars().enumerate() {
+		if c == '[' {
+			inside_brackets = true;
+		} else if c == ']' {
+			inside_brackets = false;
+			start_index = i + 1;
+		} else if !inside_brackets && i >= start_index + 3 {
+			if check_palindrome(&s[i - 3..i + 1]) {
 				return true;
 			}
 		}
@@ -56,30 +66,49 @@ fn check_abba(s: &str) -> bool {
 }
 
 fn check_tls(s: &str) -> bool {
-	if check_hypernet(s) {
-		return false;
-	}
-	check_abba(s)
+	!check_hypernet(s) && check_abba(s)
 }
 
 /* Part 2 */
 fn check_aba(s: &str) -> bool {
-	if s.len() != 3 { return false; }
-	s[0..1] == s[2..3] && s[0..1] != s[1..2]
+	s.len() == 3 && s.as_bytes()[0] == s.as_bytes()[2] && s.as_bytes()[0] != s.as_bytes()[1]
 }
 
 fn check_ssl(s: &str) -> bool {
-	let re_cut = Regex::new(r"\[.*?\]").expect("Invalid regex");
-	let abas: Vec<&str> = re_cut.split(s).collect();
-	let babs: Vec<&str> = re_cut
-		.find_iter(s)
-		.map(|m| { let ms = m.as_str(); &ms[1..ms.len()-1] })
-		.collect();
+	let mut abas = Vec::new();
+	let mut babs = Vec::new();
+	let mut inside_brackets = false;
+	let mut start_index = 0;
+
+	for (i, c) in s.chars().enumerate() {
+		if c == '[' {
+			inside_brackets = true;
+			if i > 2 {
+				abas.push(&s[start_index..i]);
+			}
+			start_index = i;
+		} else if c == ']' {
+			inside_brackets = false;
+			babs.push(&s[start_index + 1..i]);
+			start_index = i + 1;
+		} else if !inside_brackets && i >= start_index + 2 {
+			if check_aba(&s[i - 2..i + 1]) {
+				let ms_rev = inverse(&s[i - 2..i + 1]);
+				if babs.iter().any(|bab| bab.contains(&ms_rev)) {
+					return true;
+				}
+			}
+		}
+	}
+
+	if !inside_brackets && s.len() > start_index + 2 {
+		abas.push(&s[start_index..]);
+	}
 
 	for aba in abas {
-		for i in 0..aba.len()-2 {
-			if check_aba(&aba[i..i+3]) {
-				let ms_rev = inverse(&aba[i..i+3]);
+		for i in 0..aba.len() - 2 {
+			if check_aba(&aba[i..i + 3]) {
+				let ms_rev = inverse(&aba[i..i + 3]);
 				if babs.iter().any(|bab| bab.contains(&ms_rev)) {
 					return true;
 				}
@@ -90,10 +119,14 @@ fn check_ssl(s: &str) -> bool {
 }
 
 fn inverse(s: &str) -> String {
-	format!("{}{}{}", &s[1..2], &s[0..1], &s[1..2])
+	format!(
+		"{}{}{}",
+		s.as_bytes()[1] as char,
+		s.as_bytes()[0] as char,
+		s.as_bytes()[1] as char
+	)
 }
 
-fn part2(input: &str, calc: bool) -> usize {
-	if !calc { return 260; }
-	input.lines().filter(|l| check_ssl(l)).count()
+fn part2(input: &str) -> usize {
+	input.par_lines().filter(|l| check_ssl(l)).count()
 }
